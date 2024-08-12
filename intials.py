ac196fa78bcf757883238efafaf308a1c1f7f3a9
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-08-12 15:50:27 +0100

    Codecademy's Learn Python 3 First Project

---
+++ intials.py
@@ -0,0 +1,12 @@
+first_name = "Hannah"
+fun_fact = "I love Turtles"
+print("Hi my name is " + first_name + "and " + fun_fact + ". Check out my drawing!")
+
+print ("                           ")
+print ("              J J J J J     ")
+print ("            J           J   ")
+print ("    ooooo  J             J  ")
+print ("   o O O  J              J  ")
+print ("   o  ᴒ o                  ")
+print ("    ooooo J______________J  ")
+print ("            d  d     d  d   ")
